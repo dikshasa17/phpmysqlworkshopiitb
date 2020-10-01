f42d56ec17af0825a76c5de5e9d0f99848fc501a
DIKSHA SAINI <69575764+dikshasa17@users.noreply.github.com>
2020-10-01 19:54:54 +0530

    Add files via upload

--- Day-1/q1_1_evenodd.php
+++ Day-1/q1_1_evenodd.php
@@ -1,16 +1,7 @@
- <?php  
-$number1=11;
-$number2=23;
-$number3=72;
-$number4=44;
-
-if($number1%2==0)
-{
- echo "The $number1 is an even number.";
-}  
-else  
-{  
- echo "The $number1 is an odd number.";
-}
-
+<?php
+$num=44;
+if ($num%2 == 0)
+	echo "The number $num is an even number.";
+else
+    echo "The number $num is odd number.";
 ?>
