04a2b3ce87f28f33e6f5c5d682d296024348965e
DIKSHA SAINI <69575764+dikshasa17@users.noreply.github.com>
2020-10-01 18:50:05 +0530

    Add files via upload

--- Day-2/q4_2_loops.php
+++ Day-2/q4_2_loops.php
@@ -1,13 +1,8 @@
 <?php
-
-$X = array();
-
-$X[0] = "one <br/>";
-$X[1] = "two <br/>";
-$X[2] = "three <br/>";
-
-for( $i=0 ; $i<=2 ; $i++ ) {
-  print( "$X[$i]\n" );
-}
-
-?>
+ $x= array("one","two ","three");
+ 
+  foreach($x as $values)
+ {
+	 echo $values."<br>";
+ }
+ ?>
